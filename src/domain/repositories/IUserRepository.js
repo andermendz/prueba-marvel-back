bd5b775890525bd05004cb75deb4622042f7b072
andermendz <andersonmendz19@gmail.com>
2024-11-30 15:47:17 -0500

    rollback changes

--- src/domain/repositories/IUserRepository.js
+++
@@ -1,9 +0,0 @@
-class IUserRepository {
-    async findById(id) { throw new Error('Method not implemented'); }
-    async findByEmail(email) { throw new Error('Method not implemented'); }
-    async save(user) { throw new Error('Method not implemented'); }
-    async update(user) { throw new Error('Method not implemented'); }
-  }
-  
-  module.exports = IUserRepository;
-  
